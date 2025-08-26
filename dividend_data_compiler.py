251d3ff07f86694d3f3e43c1d3e1ec748fabcf4c
Yuan Jie Wong <56429440+YJwong325@users.noreply.github.com>
2025-08-25 23:27:05 -0400

    feat: Add for loop with exception handling
    The for loop displays data, and the appropriate message is printed if something goes wrong.
    
    - Data displayed is not formatted, poor readability

--- dividend_data_compiler.py
+++ dividend_data_compiler.py
@@ -4,4 +4,8 @@ tickers = yf.Tickers('AEM.TO AQN.TO ATD.TO BMO.TO BNS.TO ABX.TO BCE.TO BAM.TO BN
 
 ticker_list = ["AEM.TO", "AQN.TO", "ATD.TO", "BMO.TO", "BNS.TO", "ABX.TO", "BCE.TO", "BAM.TO", "BN.TO", "BIP.UN.TO", "CAE.TO", "CCO.TO", "CAR.UN.TO", "CM.TO", "CNR.TO", "CNQ.TO", "CP.TO", "CTC.A.TO", "CCL.B.TO", "CVE.TO", "GIB.A.TO", "CSU.TO", "DOL.TO", "EMA.TO", "ENB.TO", "FM.TO", "FSV.TO", "FTS.TO", "FNV.TO", "WN.TO", "GIL.TO", "H.TO", "IMO.TO", "IFC.TO", "K.TO", "L.TO", "MG.TO", "MFC.TO", "MRU.TO", "NA.TO", "NTR.TO", "OTEX.TO", "PPL.TO", "POW.TO", "QSR.TO", "RCI.B.TO", "RY.TO", "SAP.TO", "SHOP.TO", "SLF.TO", "SU.TO", "TRP.TO", "TECK.B.TO", "T.TO", "TRI.TO", "TD.TO", "TOU.TO", "WCN.TO", "WPM.TO", "WSP.TO"]
 
-print(len(ticker_list))
+for i in range(len(ticker_list)):
+    try:
+        print(tickers.tickers[ticker_list[i]].info)
+    except:
+        print('The data for the symbol', ticker_list[i], 'could not be successfully fetched.')
