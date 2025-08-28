0e478213587128e106cf8b141dbc4b3901af6015
Yuan Jie Wong <56429440+YJwong325@users.noreply.github.com>
2025-08-28 16:08:20 -0400

    feat: Add function to export OHLC information
     - Fetch Open, High, Low, Close (OHLC) stock values from Yahoo Finance in a specified period centered on the latest dividend pay date
     - Export OHLC data to a separate CSV file
     - Add type hints to improve clarity of yfinance function calls

--- dividend_data_compiler.py
+++ dividend_data_compiler.py
@@ -1,10 +1,11 @@
 import yfinance as yf
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta, date
+import pandas as pd
 
 def display_data(t, t_list):
     for symbol in t_list:
         try:
-            cur = t.tickers[symbol]
+            cur: yf.Ticker = t.tickers[symbol]
 
             print(f'{cur.info["longName"]} ({cur.info["symbol"]})')
             print(f'Dividend Yield (%): {cur.info["dividendYield"]}%')
@@ -18,14 +19,16 @@ def display_data(t, t_list):
 
 def export_to_csv(t, t_list):
     csv_file = open("data.csv", 'w')
-
     csv_file.write('Symbol,Div Yield,Div Pay Date,Ex-Div Date\n')
+    # csv_file.write('Symbol,Div Yield,Div Pay Date\n')
 
     for symbol in t_list:
         try:
-            cur = t.tickers[symbol]
+            cur: yf.Ticker = t.tickers[symbol]
 
             csv_file.write(f'{symbol},{cur.info["dividendRate"]} ({cur.info["dividendYield"]}%),')
+            # csv_file.write(f'{symbol},{cur.info["dividendYield"]}%,')
+
             csv_file.write(f'{cur.dividends.index[-1].date()},')
             csv_file.write(f'{datetime.fromtimestamp(cur.info["exDividendDate"], tz=timezone.utc).date()}\n')
         except:
@@ -33,14 +36,39 @@ def export_to_csv(t, t_list):
 
     csv_file.close()
 
-def main():
-    tickers = yf.Tickers('AEM.TO AQN.TO ATD.TO BMO.TO BNS.TO ABX.TO BCE.TO BAM.TO BN.TO BIP-UN.TO CAE.TO CCO.TO CAR-UN.TO CM.TO CNR.TO CNQ.TO CP.TO CTC-A.TO CCL-B.TO CVE.TO GIB-A.TO CSU.TO DOL.TO EMA.TO ENB.TO FM.TO FSV.TO FTS.TO FNV.TO WN.TO GIL.TO H.TO IMO.TO IFC.TO K.TO L.TO MG.TO MFC.TO MRU.TO NA.TO NTR.TO OTEX.TO PPL.TO POW.TO QSR.TO RCI-B.TO RY.TO SAP.TO SHOP.TO SLF.TO SU.TO TRP.TO TECK-B.TO T.TO TRI.TO TD.TO TOU.TO WCN.TO WPM.TO WSP.TO')
+def export_ohlc_to_csv(t, t_list, period):
+    # clear out csv file before appending new data
+    csv_file = open("ohlc.csv", 'w')
+    csv_file.write('Date,Ticker,Open,High,Low,Close,Volume,Dividends,Stock Splits\n')
+    csv_file.close()
 
-    ticker_list = ["AEM.TO", "AQN.TO", "ATD.TO", "BMO.TO", "BNS.TO", "ABX.TO", "BCE.TO", "BAM.TO", "BN.TO", "BIP-UN.TO", "CAE.TO", "CCO.TO", "CAR-UN.TO", "CM.TO", "CNR.TO", "CNQ.TO", "CP.TO", "CTC-A.TO", "CCL-B.TO", "CVE.TO", "GIB-A.TO", "CSU.TO", "DOL.TO", "EMA.TO", "ENB.TO", "FM.TO", "FSV.TO", "FTS.TO", "FNV.TO", "WN.TO", "GIL.TO", "H.TO", "IMO.TO", "IFC.TO", "K.TO", "L.TO", "MG.TO", "MFC.TO", "MRU.TO", "NA.TO", "NTR.TO", "OTEX.TO", "PPL.TO", "POW.TO", "QSR.TO", "RCI-B.TO", "RY.TO", "SAP.TO", "SHOP.TO", "SLF.TO", "SU.TO", "TRP.TO", "TECK-B.TO", "T.TO", "TRI.TO", "TD.TO", "TOU.TO", "WCN.TO", "WPM.TO", "WSP.TO"]
+    for symbol in t_list:
+        try:
+            cur_ticker: yf.Ticker = t.tickers[symbol]
+
+            pay_date = cur_ticker.dividends.index[-1].date()
+            period_start = pay_date - timedelta(days=period)
+            period_end = pay_date + timedelta(days=period + 1)
+
+            cur_ohlc: pd.DataFrame = cur_ticker.history(start=period_start, end=period_end, interval='1d')
+            cur_ohlc.insert(0, "Ticker", symbol)
+            cur_ohlc.to_csv("ohlc.csv", mode='a', header=False)
+        except:
+            print(f'Error writing information of {symbol} into csv file.')
+
+
+def main():
+    # Replace the placeholder ('######') in the bracket with ticker symbols of your choice delimited by spaces
+    # For example: 'AEM.TO AQN.TO ATD.TO'
+    tickers = yf.Tickers('######')
+
+    # Replace the placeholders ('######') in the array with ticker symbols that you entered in the line above
+    # For example: ["AEM.TO", "AQN.TO", "ATD.TO"]
+    ticker_list = ["######"]
 
     display_data(tickers, ticker_list)
-
     export_to_csv(tickers, ticker_list)
+    export_ohlc_to_csv(tickers, ticker_list, 7)
 
 if __name__ == "__main__":
     main()
